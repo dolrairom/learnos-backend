1301f7df220b67bf44ccb900d902ffeea25c0d71
dolrairom <lolaraigada@gmail.com>
2018-01-14 21:08:22 +0100

    error deploying

---
+++ levels.js
@@ -0,0 +1,129 @@
+const express = require('express');
+const router = express.Router();
+
+var mongodb = require('mongodb').MongoClient;
+var url = "mongodb://admin:admin@ds129386.mlab.com:29386/learnos";
+
+//Assigns levels to the new user.
+router.post('/:id', (req, res, next) => {
+  const levelsAssigned = {
+    _id: req.params.id,
+    c: "1",
+    java: "1",
+    python: "1",
+  };
+  mongodb.connect(url, function (err, client) {
+    if (err) throw err;
+    //var collection = db.collection('users');
+    var db = client.db('learnos');
+    db.collection('levels').insertOne(levelsAssigned, function(err, result) {
+      if(err){
+        console.error('Error: Unable to store levels into user with error: ', err);
+        res.status(500).send('Error: Unable to store levels into user with error: ');
+      }
+      else{
+        res.status(200).json(true);
+        console.log('Item inserted');
+        client.close();
+      }
+    });
+  });
+});
+
+//Return the current level of the user.
+router.get('/:id/:language', (req, res, next) => {
+  mongodb.connect(url, function (err, client) {
+    if (err) throw err;
+    var level;
+    var db = client.db('learnos');
+    if(req.params.language == "c"){
+      db.collection('levels').findOne({
+        _id: req.params.id
+      }, function (err, posts) {
+        if (err) {
+          res.status(500).json(err);
+        }
+        else {
+          level = posts.c;
+        }
+      });
+    }
+    else if(req.params.language == "java"){
+      db.collection('levels').findOne({
+        _id: req.params.id
+      }, function (err, posts) {
+        if (err) {
+          res.status(500).json(err);
+        }
+        else {
+          level = posts.java;
+        }
+      });
+    }
+    else if(req.params.language == "python") {
+      db.collection('levels').findOne({
+        _id: req.params.id
+      }, function (err, posts) {
+        if (err) {
+          res.status(500).json(err);
+        }
+        else {
+          level = posts.python;
+        }
+      });
+    }
+    res.status(200).json(level);
+    client.close();
+  });
+});
+
+
+//Updates the level of the user once it passes it.
+router.patch('/:id/:language/:level', (req, res, next) => {
+  var state = false;
+  mongodb.connect(url, function (err, client) {
+    if (err) throw err;
+    var WriteResult;
+    var db = client.db('learnos');
+    if(req.params.language == "c"){
+      WriteResult = db.collection('levels').update(
+        { _id: req.params.id },
+        {
+          $set:{
+            c: req.params.level
+          }
+        },
+        { upsert: true }
+      );
+    }
+    else if(req.params.language == "java"){
+      WriteResult = db.collection('levels').update(
+        { _id: req.params.id },
+        {
+          $set:{
+            java: req.params.level
+          }
+        },
+        { upsert: true }
+      );
+    }
+    else if(req.params.language == "python") {
+      WriteResult = db.collection('levels').update(
+        { _id: req.params.id },
+        {
+          $set:{
+            python: req.params.level
+          }
+        },
+        { upsert: true }
+      );
+    }
+    if(!WriteResult.writeConcernError){
+      state = true;
+      res.status(200).json(state);
+    }
+    client.close();
+  });
+});
+
+module.exports = router;
